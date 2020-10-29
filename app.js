9f14b65f7cee62ed3673946ca34c470efa9719ca
OUALI YANNICK <yo@mydatamodels.com>
2020-10-29 11:33:12 +0100

    new dependencies added

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const app = express()
+
+  
+const corsOptions = {
+    origin: [
+        'https://miage-tournament.tk',
+        'https://dev.miage-tournament.tk',
+        'http://localhost:3000',
+    ],
+}
+
+app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
+
+module.exports = app
