1b018da8cf50b03d2206583bf05589f4b537431b
OUALI YANNICK <yo@mydatamodels.com>
2020-11-14 20:31:04 +0100

    remove unused export default.

--- public/src/component/AudioLector/index.js
+++ public/src/component/AudioLector/index.js
@@ -1,12 +1,12 @@
 import template from './template.js'
 
-export default class AudioLector extends HTMLElement {
-    constructor() {
-        super()
+class AudioLector extends HTMLElement {
+  constructor() {
+    super()
 
-        this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
-    }
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
 }
 
 customElements.define('audio-lector', AudioLector)
--- public/src/component/VideoLector/index.js
+++ public/src/component/VideoLector/index.js
@@ -1,69 +1,69 @@
 import template from './template.js'
 
-export default class VideoLector extends HTMLElement {
-    constructor() {
-        super()
+class VideoLector extends HTMLElement {
+  constructor() {
+    super()
 
-        this.initAttribute()
+    this.initAttribute()
 
-        this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
 
-        this.initQuerySelectors().then(() => {
-            this.source.src = this.srcAttribute
-            this.shadowRoot.querySelector('.title').innerText = this.titleAttribute
-            this.initListeners()
-        })
+    this.initQuerySelectors().then(() => {
+      this.source.src = this.srcAttribute
+      this.shadowRoot.querySelector('.title').innerText = this.titleAttribute
+      this.initListeners()
+    })
+  }
+
+  initAttribute = () => {
+    this.loopAttribute = this.getAttribute('loop')
+    this.srcAttribute = this.getAttribute('src')
+    this.titleAttribute = this.getAttribute('titre')
+  }
+
+  initQuerySelectors = async () => {
+    this.button = {
+      play: this.shadowRoot.querySelector('#play'),
+      pause: this.shadowRoot.querySelector('#pause'),
+      stop: this.shadowRoot.querySelector('#stop'),
+      avance: this.shadowRoot.querySelector('#avance'),
+      recul: this.shadowRoot.querySelector('#recul'),
+      loop: this.shadowRoot.querySelector('#loop'),
     }
+    this.videoLector = this.shadowRoot.querySelector('.video-lector')
+    this.source = this.shadowRoot.querySelector('.video-lector source')
+  }
 
-    initAttribute = () => {
-        this.loopAttribute = this.getAttribute('loop')
-        this.srcAttribute = this.getAttribute('src')
-        this.titleAttribute = this.getAttribute("titre")
-    }
+  initListeners = () => {
+    this.button.play.addEventListener('click', () => {
+      this.videoLector.play()
+    })
 
-    initQuerySelectors = async () => {
-        this.button = {
-            play: this.shadowRoot.querySelector('#play'),
-            pause: this.shadowRoot.querySelector('#pause'),
-            stop: this.shadowRoot.querySelector('#stop'),
-            avance: this.shadowRoot.querySelector('#avance'),
-            recul: this.shadowRoot.querySelector('#recul'),
-            loop: this.shadowRoot.querySelector('#loop'),
-        }
-        this.videoLector = this.shadowRoot.querySelector('.video-lector')
-        this.source = this.shadowRoot.querySelector('.video-lector source')
-    }
+    this.button.pause.addEventListener('click', () => {
+      this.videoLector.pause()
+    })
 
-    initListeners = () => {
-        this.button.play.addEventListener('click', () => {
-            this.videoLector.play()
-        })
+    this.button.stop.addEventListener('click', () => {
+      this.videoLector.currentTime = 0
+      this.videoLector.pause()
+    })
 
-        this.button.pause.addEventListener('click', () => {
-            this.videoLector.pause()
-        })
+    this.button.avance.addEventListener('click', () => {
+      this.videoLector.currentTime += 10
+    })
 
-        this.button.stop.addEventListener('click', () => {
-            this.videoLector.currentTime = 0
-            this.videoLector.pause()
-        })
+    this.button.recul.addEventListener('click', () => {
+      this.videoLector.currentTime -= 10
+    })
 
-        this.button.avance.addEventListener('click', () => {
-            this.videoLector.currentTime += 10
-        })
-
-        this.button.recul.addEventListener('click', () => {
-            this.videoLector.currentTime -= 10
-        })
-
-        this.button.loop.addEventListener('click', () => {
-            this.videoLector.loop = !this.videoLector.loop
-            this.button.loop.style.backgroundColor = this.videoLector.loop
-                ? '#33FF3380'
-                : '#FF333380'
-        })
-    }
+    this.button.loop.addEventListener('click', () => {
+      this.videoLector.loop = !this.videoLector.loop
+      this.button.loop.style.backgroundColor = this.videoLector.loop
+        ? '#33FF3380'
+        : '#FF333380'
+    })
+  }
 }
 
 customElements.define('video-player', VideoLector)
