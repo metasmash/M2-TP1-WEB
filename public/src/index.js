f56f12045785db0452ce50a947026302f9f8535e
OUALI YANNICK <yo@mydatamodels.com>
2020-11-14 14:45:47 +0100

    using index.js as a default module for every of our components.

--- public/src/index.js
+++ public/src/index.js
@@ -1,43 +1,5 @@
+// Here, we use index.js to import every component and use it as a module.
+// This module is imported inside index.html, so we can freely use every custom web component :-)
+
 import './component/VideoLector/index.js'
 import './component/AudioLector/index.js'
-
-const button = {
-    play: document.querySelector('#play'),
-    pause: document.querySelector('#pause'),
-    stop: document.querySelector('#stop'),
-    avance: document.querySelector('#avance'),
-    recul: document.querySelector('#recul'),
-    loop: document.querySelector('#loop'),
-}
-
-const videoLector = document.querySelector('#mainvid')
-
-console.log(videoLector)
-
-button.play.addEventListener('click', () => {
-    videoLector.play()
-})
-button.pause.addEventListener('click', () => {
-    videoLector.pause()
-})
-
-button.stop.addEventListener('click', () => {
-    videoLector.currentTime = 0
-    videoLector.pause()
-})
-
-button.avance.addEventListener('click', () => {
-    videoLector.currentTime += 10
-})
-
-button.recul.addEventListener('click', () => {
-    videoLector.currentTime -= 10
-})
-
-button.loop.addEventListener('click', () => {
-    videoLector.loop = !videoLector.loop
-    console.log(true)
-    button.loop.style.backgroundColor = videoLector.loop
-        ? '#33FF3380'
-        : '#FF333380'
-})
