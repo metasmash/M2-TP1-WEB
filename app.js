137fea6145de72b00c00c21c21fe1427712772ec
OUALI YANNICK <yo@mydatamodels.com>
2020-11-14 20:30:50 +0100

    prettier applyied

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require( 'express')
-const bodyParser = require( 'body-parser')
-const cors = require( 'cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000'],
 }
 
 app.use(cors(corsOptions))
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
 })
 
 // parse requests of content-type - application/json
@@ -27,11 +27,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // set port, listen for requests
 const PORT = process.env.PORT || 9991
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
+  console.log(`Server is running on port ${PORT}.`)
 })
 
 app.get('/', (req, res) => {
-    const pathToHTML = `${__dirname}\\public\\index.html`
+  const pathToHTML = `${__dirname}\\public\\index.html`
 
-    res.sendFile(pathToHTML)
+  res.sendFile(pathToHTML)
 })
