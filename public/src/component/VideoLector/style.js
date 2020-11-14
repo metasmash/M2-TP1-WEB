1cbb63a0c4822e9ecf2839c19eabce03d399413d
OUALI YANNICK <yo@mydatamodels.com>
2020-11-14 22:09:58 +0100

    css is now outside of the template module.

---
+++ public/src/component/VideoLector/style.js
@@ -0,0 +1,49 @@
+export default `<style> 
+#mainvid {
+    width:1080px;
+    height:500px;
+}
+
+:focus { outline: none; }
+
+#root {
+    display:flex;
+    margin-bottom: 5%;
+    margin-left:30px;
+    margin-top:30px;
+}
+
+button {
+    margin:50px;
+    width:100px;
+    height:100px;
+    -webkit-border-radius: 100px;
+    -moz-border-radius: 100px;
+    border-radius: 100px;
+    border: solid #FFFFA2;
+}
+
+#buttonGroup {
+    display:flex;
+    flex-flow: row wrap;
+}
+
+h1 {
+    position:absolute;
+    margin-top:-10px;
+    color:white;
+}
+
+#loop {
+    background-color: #33FF3380;
+}
+
+.video-lector {
+    width:720px;
+    height:480px;
+    background-color: black;
+    border: 1px solid #ffffff;
+   
+}
+
+</style>`
--- public/src/component/VideoLector/template.js
+++ public/src/component/VideoLector/template.js
@@ -1,55 +1,7 @@
 const template = document.createElement('template')
+import css from './style'
 
-template.innerHTML = `
-<style> 
-#mainvid {
-    width:1080px;
-    height:500px;
-}
-
-:focus { outline: none; }
-
-#root {
-    display:flex;
-    margin-bottom: 5%;
-    margin-left:30px;
-    margin-top:30px;
-}
-
-button {
-    margin:50px;
-    width:100px;
-    height:100px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-    border-radius: 100px;
-    border: solid #FFFFA2;
-}
-
-#buttonGroup {
-    display:flex;
-    flex-flow: row wrap;
-}
-
-h1 {
-    position:absolute;
-    margin-top:-10px;
-    color:white;
-}
-
-#loop {
-    background-color: #33FF3380;
-}
-
-.video-lector {
-    width:720px;
-    height:480px;
-    background-color: black;
-    border: 1px solid #ffffff;
-   
-}
-
-</style>
+template.innerHTML = `${css}
 <main>
 <div id="root">
 <h1 class="title"></h1>
