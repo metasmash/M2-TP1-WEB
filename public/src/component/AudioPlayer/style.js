89f4900501755a9a0142139c9f5ff171e9b5558e
OUALI YANNICK <yo@mydatamodels.com>
2020-11-15 19:00:41 +0100

    new style for our component :)

--- public/src/component/AudioPlayer/style.js
+++ public/src/component/AudioPlayer/style.js
@@ -6,9 +6,29 @@ export default `
     margin-left:30px;
     margin-top:30px;
     color:white;
+    background-color: midnightblue;
+    width:auto;
+    height:auto;
+    display: inline-block;
+    padding:10px;
+    border-radius: 10px;
+    border: solid #d3fffd;
 }
 
 :focus { outline: none; }
+
+#equalizer {
+    display:flex;
+    flex-flow: row wrap;
+}
+
+#equalizer-inputs {
+    margin-left:10px;
+    margin-top:5px;
+}
+
+#title {
+}
 
 
 </style>
