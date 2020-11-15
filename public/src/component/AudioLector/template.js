b353c43aef023c859eb69e7f8ebe61774cc0996f
OUALI YANNICK <yo@mydatamodels.com>
2020-11-15 01:31:18 +0100

    update audio element template.

--- public/src/component/AudioLector/template.js
+++ public/src/component/AudioLector/template.js
@@ -3,8 +3,11 @@ import css from './style.js'
 
 template.innerHTML = `${css}
 <div id="root">
-    <audio class="audio-element" src="" controls></audio>
-
+    <audio controls crossOrigin="anonymous" class="audio-element" src=""></audio>
+    <button id="play">Play</button>
+    <button id="pause">Pause</button>
+    Panner: <input id="panner" type="range" min="-1" max="1" value="0" step="0.01">
+    Audio: <input id="gain" type="range" min="0" max="1" value="1" step="0.01">
 </div>
 `
 
