77fab369c0d31864047b95239e6d8dc996d1d4b0
OUALI YANNICK <yo@mydatamodels.com>
2020-11-15 20:23:02 +0100

    add frequencies visualization

--- public/src/component/AudioPlayer/index.js
+++ public/src/component/AudioPlayer/index.js
@@ -12,9 +12,11 @@ class AudioLector extends HTMLElement {
 
         this.equalizerValues = [60, 170, 350, 1000, 3500, 10000]
         this.filters = []
-        this.fftSize = 1024
+        this.fftSizeWaveForm = 1024
+        this.fftSizeFrequencies = 256
 
-        this.init().then(() => {
+        this.init().then(async () => {
+            await this.initDraw()
             console.log('Init is a success!!')
         })
     }
@@ -51,8 +53,11 @@ class AudioLector extends HTMLElement {
         this.filters[this.filters.length - 1].connect(this.panNode)
 
         this.panNode.connect(this.gainNode)
-        this.gainNode.connect(this.analyser)
-        this.analyser.connect(this.dest)
+        this.gainNode.connect(this.dest)
+
+        //connect to visualizer
+        this.gainNode.connect(this.analyserWaveForm)
+        this.gainNode.connect(this.analyserFrequencies)
     }
 
     initAudioNodes = () => {
@@ -61,10 +66,22 @@ class AudioLector extends HTMLElement {
 
         this.gainNode = this.audioContext.createGain()
         this.panNode = this.audioContext.createStereoPanner()
-        this.analyser = this.audioContext.createAnalyser()
-        this.analyser.fftSize = this.fftSize
-        this.analyser.bufferLength = this.analyser.frequencyBinCount
-        this.analyser.dataArray = new Uint8Array(this.analyser.bufferLength)
+
+        //for waveForm
+        this.analyserWaveForm = this.audioContext.createAnalyser()
+        this.analyserWaveForm.fftSize = this.fftSizeWaveForm
+        this.analyserWaveForm.bufferLength = this.analyserWaveForm.frequencyBinCount
+        this.analyserWaveForm.dataArray = new Uint8Array(
+            this.analyserWaveForm.bufferLength
+        )
+
+        //for frequenciesVisualization
+        this.analyserFrequencies = this.audioContext.createAnalyser()
+        this.analyserFrequencies.fftSize = this.fftSizeFrequencies
+        this.analyserFrequencies.bufferLength = this.analyserFrequencies.frequencyBinCount
+        this.analyserFrequencies.dataArray = new Uint8Array(
+            this.analyserFrequencies.bufferLength
+        )
 
         this.initEqualizer()
     }
@@ -107,7 +124,10 @@ class AudioLector extends HTMLElement {
             pause: this.shadowRoot.querySelector('#pause'),
         }
         this.title = this.shadowRoot.querySelector('#title')
-        this.canvas = this.shadowRoot.querySelector('#wave-form')
+        this.canvasWaveForm = this.shadowRoot.querySelector('#wave-form')
+        this.canvasFrequencies = this.shadowRoot.querySelector(
+            '#frequencies-visualization'
+        )
     }
 
     play = () => {
@@ -130,32 +150,32 @@ class AudioLector extends HTMLElement {
             eq.gain.value = 0
             this.filters.push(eq)
         })
-
-        requestAnimationFrame(this.initWaveForm)
     }
 
-    initWaveForm = () => {
-        let width = this.canvas.width
-        let height = this.canvas.height
-        let canvasContext = this.canvas.getContext('2d')
+    drawWaveForm = () => {
+        let width = this.canvasWaveForm.width
+        let height = this.canvasWaveForm.height
+        let canvasContext = this.canvasWaveForm.getContext('2d')
 
         canvasContext.fillStyle = 'rgba(0, 0, 0, 0.5)'
         canvasContext.fillRect(0, 0, width, height)
 
-        this.analyser.getByteTimeDomainData(this.analyser.dataArray)
+        this.analyserWaveForm.getByteTimeDomainData(
+            this.analyserWaveForm.dataArray
+        )
 
         canvasContext.lineWidth = 2
         canvasContext.strokeStyle = 'red'
 
         canvasContext.beginPath()
 
-        let sliceWidth = width / this.analyser.bufferLength
+        let sliceWidth = width / this.analyserWaveForm.bufferLength
         let x = 0
 
-        for (let i = 0; i < this.analyser.bufferLength; i++) {
+        for (let i = 0; i < this.analyserWaveForm.bufferLength; i++) {
             // dataArray values are between 0 and 255,
             // normalize v, now between 0 and 1
-            let v = this.analyser.dataArray[i] / 255
+            let v = this.analyserWaveForm.dataArray[i] / 255
             // y will be in [0, canvas height], in pixels
             let y = v * height
 
@@ -168,11 +188,60 @@ class AudioLector extends HTMLElement {
             x += sliceWidth
         }
 
-        canvasContext.lineTo(this.canvas.width, this.canvas.height / 2)
+        canvasContext.lineTo(
+            this.canvasWaveForm.width,
+            this.canvasWaveForm.height / 2
+        )
 
         canvasContext.stroke()
 
-        requestAnimationFrame(this.initWaveForm)
+        requestAnimationFrame(this.drawWaveForm)
+    }
+
+    drawFrequencies = () => {
+        let width = this.canvasFrequencies.width
+        let height = this.canvasFrequencies.height
+        let canvasContext = this.canvasFrequencies.getContext('2d')
+
+        // clear the canvas
+        canvasContext.clearRect(0, 0, width, height)
+        // Get the analyser data
+        this.analyserFrequencies.getByteFrequencyData(
+            this.analyserFrequencies.dataArray
+        )
+
+        let barWidth = width / this.analyserFrequencies.bufferLength
+        let barHeight
+        let x = 0
+        // values go from 0 to 255 and the canvas heigt is 100. Let's rescale
+        // before drawing. This is the scale factor
+        let heightScale = height / 128
+        for (let i = 0; i < this.analyserFrequencies.bufferLength; i++) {
+            // between 0 and 255
+            barHeight = this.analyserFrequencies.dataArray[i]
+
+            // The color is red but lighter or darker depending on the value
+            canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
+            // scale from [0, 255] to the canvas height [0, height] pixels
+            barHeight *= heightScale
+            // draw the bar
+            canvasContext.fillRect(
+                x,
+                height - barHeight / 2,
+                barWidth,
+                barHeight / 2
+            )
+
+            // 1 is the number of pixels between bars - you can change it
+            x += barWidth + 1
+        }
+        // once again call the visualize function at 60 frames/s
+        requestAnimationFrame(this.drawFrequencies)
+    }
+
+    initDraw = async () => {
+        requestAnimationFrame(this.drawWaveForm)
+        requestAnimationFrame(this.drawFrequencies)
     }
 }
 
