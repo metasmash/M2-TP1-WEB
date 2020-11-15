649f96223fd1a2cd5eef42840458b6448dfc4ea1
OUALI YANNICK <yo@mydatamodels.com>
2020-11-15 01:41:08 +0100

    refactoring name of directory.

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,5 @@
 // Here, we use index.js to import every component and use it as a module.
 // This module is imported inside index.html, so we can freely use every custom web component :-)
 
-import './component/VideoLector/index.js'
-import './component/AudioLector/index.js'
+import './component/VideoPlayer/index.js'
+import './component/AudioPlayer/index.js'
