8da816805adefe7c5fe01e3e840cb26d58ccc7bb
OUALI YANNICK <yo@mydatamodels.com>
2020-11-16 01:39:53 +0100

    fix bug/troubles with id of labels. add a wrapper for canvas.

--- public/src/component/AudioPlayer/template.js
+++ public/src/component/AudioPlayer/template.js
@@ -13,32 +13,38 @@ template.innerHTML = `${css}
     <div id="equalizer">
 <div id="equalizer-inputs">
     <label id="hz-value" for="eq-1">Band 60Hz:   </label>
-     <input id="eq-1" type="range" min="-20" max="20" value="0" step="0.01">
+     <input id="eq-in-1" type="range" min="-20" max="20" value="0" step="0.01">
      <label id="eq-value" for="eq-1">0</label>
      <br>   
       <label id="hz-value" for="eq-2">Band 170Hz:  </label>
-     <input id="eq-2" type="range" min="-20" max="20" value="0" step="0.01">
+     <input id="eq-in-2" type="range" min="-20" max="20" value="0" step="0.01">
      <label id="eq-value" for="eq-2">0</label>
      <br>
          <label id="hz-value" for="eq-3">Band 350Hz:  </label>
-     <input id="eq-3" type="range" min="-20" max="20" value="0" step="0.01">
+     <input id="eq-in-3" type="range" min="-20" max="20" value="0" step="0.01">
      <label id="eq-value" for="eq-3">0</label>
      <br>
          <label id="hz-value" for="eq-4">Band 1000Hz: </label>
-      <input id="eq-4" type="range" min="-20" max="20" value="0" step="0.01">
+      <input id="eq-in-4" type="range" min="-20" max="20" value="0" step="0.01">
       <label id="eq-value" for="eq-4">0</label>
       <br>
           <label id="hz-value" for="eq-5">Band 3500Hz: </label>
-      <input id="eq-5" type="range" min="-20" max="20" value="0" step="0.01">
+      <input id="eq-in-5" type="range" min="-20" max="20" value="0" step="0.01">
       <label id="eq-value" for="eq-5">0</label>
       <br>
           <label id="hz-value" for="eq-6">Band 10000Hz:</label>
-      <input id="eq-6" type="range" min="-20" max="20" value="0" step="0.01">
+      <input id="eq-in-6" type="range" min="-20" max="20" value="0" step="0.01">
           <label id="eq-value" for="eq-6">0</label>
     </div>
     </div>
-    <canvas id="wave-form"></canvas><br>
+     <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label><br>
+<div class="wrapper">
+    <canvas id="wave-form"></canvas>
     <canvas id="frequencies-visualization"></canvas><br>
+</div>
     <a href="https://learning.edx.org/course/course-v1:W3Cx+HTML5.2x+2T2020a/block-v1:W3Cx+HTML5.2x+2T2020a+
     type@sequential+block@f162bb287eca4f04bb22d60b2c5456ac/block-v1:W3Cx+HTML5.2x+2T2020a+type@verti
     cal+block@c48cf5b33ed24756b362b749192840ff">
